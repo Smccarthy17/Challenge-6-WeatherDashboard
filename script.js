42c2b977d26ed4c97105c0117294ed640d0cacae
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-06-04 22:04:57 -0400

    create fetch resonse1, working on fetch response2

---
+++ script.js
@@ -0,0 +1,52 @@
+var searchButton = document.getElementById('searchButton');
+var searchCity = document.getElementById('city');
+const apiKey = 'cc14ffc03b442925b564b8a1b5bba2c9';
+
+console.log(searchButton);
+searchButton.addEventListener('click', function () {
+    console.log(searchCity.value);
+    displayWeather();
+});
+
+// funtions to fetch API's to show present and future weather
+function displayWeather() {
+    var city = searchCity.value;
+    var request1URL =
+        'https://api.openweathermap.org/data/2.5/weather?q=' +
+        city +
+        ',us&units=imperial&appid=' +
+        apiKey;
+    var request2URL =
+        'http://api.openweathermap.org/data/2.5/onecall?lat=' +
+        lat +
+        '&lon=' +
+        long +
+        '&units=imperial&appid=' +
+        apiKey;
+
+    fetch(request1URL)
+        .then(function (response1) {
+            console.log(response1);
+            return response1.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var currentTempEl = document.getElementById('temp');
+            currentTempEl.textContent = data.main.temp;
+            currentTempEl.textContent = data.wind.speed;
+            currentTempEl.textContent = data.main.humidity;
+            console.log(data.main.temp);
+            console.log(data.wind.speed);
+            console.log(data.main.humidity);
+        });
+
+    fetch(request2URL).then(function (response2) {
+        console.log(response2);
+        return response2.json();
+    });
+
+    // .then(function (data) {
+    // console.log(data);
+    // var currentUVI = document.getElementById('')
+    // })
+}
